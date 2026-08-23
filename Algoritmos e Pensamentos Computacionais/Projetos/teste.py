2e9c164988fd8629151acba4b1e9c4edc5034cee
Estevão <loyddformal@gmail.com>
2026-08-22 22:38:20 -0300

    calculo de média

---
+++ Algoritmos e Pensamentos Computacionais/Projetos/teste.py
@@ -0,0 +1,13 @@
+
+print("\n Calcúlo de notas + ")
+
+
+n1 = float(input("Digite a nota da A2: "))
+n2 = float(input("Digite a nota da A1: "))
+n3 = (n1 + n2) 
+print("resultado", n3)
+
+if n3 >= 6:
+    print("Aprovado")
+else:
+    print("Reprovado")
