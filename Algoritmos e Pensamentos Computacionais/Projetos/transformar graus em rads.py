992a17a60d6de76dc1fb768a8620c7d1262fac07
Estevão <loyddformal@gmail.com>
2026-08-22 22:35:08 -0300

    python

---
+++ Algoritmos e Pensamentos Computacionais/Projetos/transformar graus em rads.py
@@ -0,0 +1,6 @@
+print("--------------------------------")
+print("Conversor de graus para fahrenheit")
+print("--------------------------------")
+g = float(input("Digite o valor em graus: "))
+f = g * 9 /5 + 32
+print(f"O valor em fahrenheit é: {f}")
